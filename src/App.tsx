19c0d236a3696bb2881512a701fa4d85347c1e16
pdpau <56670577+pdpau@users.noreply.github.com>
2024-06-24 19:58:44 +0200

    reorganized main

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,19 @@ function App() {
 
   return (
     <>
-      <Header />
+      <body className="flex justify-center items-center"> {/* ¿¿Mejor usar div?? */}
+        <main className="w-[450px]">
+          <Header />
 
-      <Separator />
+          <Separator className="bg-my-red-50"/>
 
-      <Pomodoro />
+          <Pomodoro />
+
+          <Separator className="bg-my-red-50"/>
+
+          {/* Here goes the todo list */}
+        </main>
+      </body>
     </>
   )
 }
