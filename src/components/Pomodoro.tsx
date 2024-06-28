a57ea16dff3affd1df8503c85d7b9b35a96c9369
pdpau <56670577+pdpau@users.noreply.github.com>
2024-06-28 21:07:15 +0200

    ADD timer functionality

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -1,25 +1,15 @@
-import { Button } from '@/components/ui/button';
+
+import { Separator } from '@/components/ui/separator';
 import { cn } from '@/lib/utils';
-import { ModeType, PomodoroOptionsType } from '@/types';
+import { ModeType } from '@/types';
+import { format } from 'path';
 
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
-const PomodoroOptions = {
-    '25/5': {
-        /* work: 3, // in seconds
-        rest: 5 */
-        initWork: 25 * 60,
-        initRest: 5 * 60
-    },
-    '50/10': {
-        initWork: 50 * 60,
-        initRest: 10 * 60
-    }
-}
-
-const formatTime = (time: number, setDisplay: Dispatch<SetStateAction<string>>) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
+const formatTime = (minutesInput: number, setDisplay: Dispatch<SetStateAction<string>>) => {
+    const totalSeconds = Math.floor(minutesInput * 60);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
 
     const strMinutes = minutes.toString().padStart(2, '0');
     const strSeconds = seconds.toString().padStart(2, '0');
@@ -27,32 +17,29 @@ const formatTime = (time: number, setDisplay: Dispatch<SetStateAction<string>>) 
     setDisplay(`${strMinutes}:${strSeconds}`);
 }
 
+interface PomodoroProps {
+    pomodoroTime: number;
+    setPomodoroTime: Dispatch<SetStateAction<number>>;
+    shortBreakTime: number;
+    setShortBreakTime: Dispatch<SetStateAction<number>>;
+    longBreakTime: number;
+    setLongBreakTime: Dispatch<SetStateAction<number>>;
+}
 
-const Pomodoro = () => {
-    /* Options and modes */
-    const [option, setOption] = useState<PomodoroOptionsType>('25/5');
-    const [work, setWork] = useState<number>(PomodoroOptions[option].initWork);
-    const [rest, setRest] = useState<number>(PomodoroOptions[option].initRest);
+const Pomodoro: React.FC<PomodoroProps> = ({pomodoroTime, setPomodoroTime, shortBreakTime, setShortBreakTime, longBreakTime, setLongBreakTime}) => {
+    const [temporalPomodoroTime, setTemporalPomodoroTime] = useState<number>(pomodoroTime);
+    const [temporalShortBreakTime, setTemporalShortBreakTime] = useState<number>(shortBreakTime);
+    const [temporalLongBreakTime, setTemporalLongBreakTime] = useState<number>(longBreakTime);
+    const handleRestart = () => {
+        setTemporalPomodoroTime(pomodoroTime);
+        setTemporalShortBreakTime(shortBreakTime);
+        setTemporalLongBreakTime(longBreakTime);
+    }
+
+
     const [mode, setMode] = useState<ModeType>('work');
-    /* const handleOption = (newOption: PomodoroOptionsType) => {
-        if (mode === 'work') setWork(PomodoroOptions[newOption].initWork);
-        if (mode === 'rest') setRest(PomodoroOptions[newOption].initRest);
-        setOption(newOption);
-    }
-    const handleMode = (newMode: ModeType) => {
-        if (newMode === 'work') setWork(PomodoroOptions[option].initWork);
-        if (newMode === 'rest') setRest(PomodoroOptions[option].initRest);
-        setMode(newMode);
-    } */
-    const handleOptionMode = (newOption: PomodoroOptionsType, newMode: ModeType) => {
-        setOption(newOption);
-        setMode(newMode);
-        setWork(PomodoroOptions[newOption].initWork);
-        setRest(PomodoroOptions[newOption].initRest);
-    }
-    /* End of options and modes */
 
-    /* Playing or not playing time */
+    /* ---------- Playing or not playing time ---------- */
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
     const handleIsPlaying = () => {
         setIsPlaying(!isPlaying);
@@ -61,57 +48,109 @@ const Pomodoro = () => {
         if (!isPlaying) return;
         const interval = setInterval(() => {
             if (mode === 'work') {
-                setWork(work => work - 1);
-                if (work <= 0) {
-                    handleOptionMode(option, 'rest');
-                    formatTime(rest, setDisplay); // para que se actualice el display a los minutos antes de empezar la cuenta atras
+                setTemporalPomodoroTime(temporalPomodoroTime => temporalPomodoroTime - (1/60));
+                if (temporalPomodoroTime <= (1/60)) {
+                    setMode('rest');
+                    handleRestart();
                 }
             } else if (mode === 'rest') {
-                setRest(rest => rest - 1);
-                if (rest <= 0) {
-                    handleOptionMode(option, 'work');
-                    formatTime(work, setDisplay);
+                setTemporalShortBreakTime(temporalShortBreakTime => temporalShortBreakTime - (1/60));
+                if (temporalShortBreakTime <= (1/60)) {
+                    setMode('work');
+                    handleRestart();
                 }
             }
         }, 1000);
         return () => clearInterval(interval);
-    }, [isPlaying, work, rest, mode]);
-    /*  End of playing or not playing time */
+    }, [isPlaying, mode, temporalPomodoroTime, temporalShortBreakTime])
+    /* ---------- End of playing or not playing time ---------- */
 
-    /* Time display */
+    /* ---------- Time display ---------- */
     const [display, setDisplay] = useState<string>('00:00');
     useEffect(() => {
         if (mode !== 'work') return;
-        formatTime(work, setDisplay);
-    }, [work]);
+        formatTime(temporalPomodoroTime, setDisplay); /* TODO: Falta que quan li doni a SAVE es mostri els nous minuts al timer */
+    }, [temporalPomodoroTime]);
     useEffect(() => {
         if (mode !== 'rest') return;
-        formatTime(rest, setDisplay);
-    }, [rest]);
-    /* End of time display */
+        formatTime(temporalShortBreakTime, setDisplay);
+    }, [temporalShortBreakTime]);
+    /* ---------- End of time display ---------- */
+
+    /* ---------- Handle WORK and BREAK buttons ---------- */
+    const handleMode = (newMode: ModeType) => {
+        if (newMode === 'work' && mode === 'rest') {
+            setTemporalPomodoroTime(pomodoroTime);
+            setTemporalShortBreakTime(shortBreakTime);
+            setTemporalLongBreakTime(longBreakTime);
+        }
+        if (newMode === 'rest' && mode === 'work') {
+            setTemporalPomodoroTime(pomodoroTime);
+            setTemporalShortBreakTime(shortBreakTime);
+            setTemporalLongBreakTime(longBreakTime);
+        }
+        setMode(newMode); /* TODO: Segueix el mateix problema al canviar de modes */
+    }
+    /* ---------- End of handle WORK and BREAK buttons ---------- */
+
+
+
+
 
     return (
-        <section className={cn("w-full h-full",
-        "gap-8 p-8",
-        "flex flex-col items-center justify-center"
-        )}>
-            <div className="flex gap-4"> {/* TODO: Passar al header */}
-                <Button onClick={() => handleOptionMode("25/5", mode)} variant={'default'}>25/5</Button>
-                <Button onClick={() => handleOptionMode("50/10", mode)} variant={'secondary'} className="hover:bg-slate-200">50/10</Button>
+        <section className={cn("w-full h-full p-8", "flex items-center justify-between", "text-my-red-950")}>
+            {/* Time */}
+            <div className={cn("min-w-80 min-h-56 max-w-80 max-h-80", 
+                "flex flex-col items-center justify-center rounded-lg", 
+                "bg-white bg-opacity-10"
+            )}>
+                <div id="options" className="flex gap-4">
+                    <button className={cn()} onClick={() => handleMode('work')}>Work</button>
+                    <button className={cn()} onClick={() => handleMode('rest')}>Break</button>
+                </div>
+                <span className="text-xl font-bold">{mode === 'work' ? 'Working' : 'Break time !!'}</span>
+                <span className="text-8xl font-bold">{display}</span>
+                <div id="start-stop-buttons" className="flex gap-2">
+                    {/* TODO: Donar estils als botons */}
+                    {isPlaying ? (
+                        <button className={cn("w-16 h-8 p-1.5 rounded-sm", 
+                            "bg-my-red-400",
+                            "transition duration-300",
+                            "hover:bg-my-red-500 hover:scale-105")}
+                            onClick={handleIsPlaying}
+                        >Stop</button>
+                    ) : (
+                        <div>
+                            <button className={cn("w-16 h-8 p-1.5 rounded-sm", 
+                                "bg-my-red-400",
+                                "transition duration-300",
+                                "hover:bg-my-red-500 hover:scale-105")}
+                                onClick={handleIsPlaying}
+                            >Start</button>
+                            <button className={cn("w-16 h-8 p-1.5 rounded-sm", 
+                                "bg-my-red-400",
+                                "transition duration-300",
+                                "hover:bg-my-red-500 hover:scale-105")}
+                                onClick={handleRestart}
+                            >Restart</button>
+                        </div>
+                    )}
+                </div>
             </div>
-            <div id="options" className="flex gap-4">
-                <Button onClick={() => handleOptionMode(option, "work")} variant={'secondary'} className="hover:bg-slate-200">Work</Button>
-                <Button onClick={() => handleOptionMode(option, "rest")} variant={'default'}>Break</Button>
-            </div>
-            <span className="text-3xl font-bold">{mode === 'work' ? 'Working' : 'Break time !!'}</span>
-            <span className="text-8xl font-bold">{display}</span>
-            <div id="start-stop-buttons" className="flex gap-2">
-                {/* TODO: Donar estils als botons */}
-                {isPlaying ? (
-                    <Button onClick={handleIsPlaying} variant={'destructive'}>Stop</Button>
-                ) : (
-                    <Button onClick={handleIsPlaying} variant={'default'}>Start</Button>
-                )}
+
+            <Separator orientation='vertical' className='bg-white' />
+
+            {/* Tasks */}
+            <div className={cn("min-w-80 min-h-56 max-w-80 max-h-80", 
+                "flex flex-col items-center justify-center rounded-lg", 
+                "bg-white bg-opacity-10"
+            )}>
+                <div className="flex justify-center items-center pb-1">
+                    <h3 className="text-sm text-black font-playfair font-bold border-b border-black">THIS WILL BE A TASKS SECTION THAT WILL ALLOW THE USER TO CREATE A TODO LIST FOR HIS/HER WORK SESSION</h3>
+                </div>
+                <div className="flex justify-center">
+                    <button>TODO</button>
+                </div>
             </div>
         </section>
     )
