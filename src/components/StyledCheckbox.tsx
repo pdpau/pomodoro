3792374d64f085cb854291e0d2987604d028367e
pdpau <56670577+pdpau@users.noreply.github.com>
2024-07-04 20:09:51 +0200

    change color to my-red-950

--- src/components/StyledCheckbox.tsx
+++ src/components/StyledCheckbox.tsx
@@ -51,11 +51,11 @@ const StyledCheckbox: React.FC<StyledCheckboxProps> = ({ isChecked, handleCheckb
             />
             <label className="flex items-center justify-center space-x-2 cursor-pointer">
                 <svg width="40" height="40" viewBox="0 0 90 90">
-                    <rect x="20" y="20" width="45" height="45" stroke="black" strokeWidth="2.5" fill="none"/>
+                    <rect x="20" y="20" width="45" height="45" stroke="#400f0f" strokeWidth="2.5" fill="none"/>
                     <g transform="translate(0,-940)">
                         <path
                             d="m 50,950 c -75,95 5,7 6,7 12,-4 -50,60 -30,45 95,-65 -5,10 15,3 23,-9 35,-35 18,-4 "
-                            stroke="black"
+                            stroke="#400f0f"
                             strokeWidth="2.5"
                             fill="none"
                             className={`path1 transition-all duration-500 stroke-dasharray-350 ${isChecked ? 'stroke-dashoffset-0 opacity-100' : 'stroke-dashoffset-350 opacity-0'}`}
