7dea4f4e57059530b22eb14adc8e92afd05ad3f4
pdpau <56670577+pdpau@users.noreply.github.com>
2024-07-29 17:58:06 +0200

    FILTER_BUTTONS

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,18 @@ export const TASK_FILTERS = {
     ACTIVE: 'active',
     COMPLETED: 'completed'
 } as const;
+
+export const FILTER_BUTTONS = {
+    [TASK_FILTERS.ALL]: {
+        literal: 'All',
+        href: `?filter=${TASK_FILTERS.ALL}`
+    },
+    [TASK_FILTERS.ACTIVE]: {
+        literal: 'Active',
+        href: `?filter=${TASK_FILTERS.ACTIVE}`
+    },
+    [TASK_FILTERS.COMPLETED]: {
+        literal: 'Completed',
+        href: `?filter=${TASK_FILTERS.COMPLETED}`
+    }
+} as const;
