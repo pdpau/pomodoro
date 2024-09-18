5243c7621347a95be880183b2a6a49412d427475
pdpau <56670577+pdpau@users.noreply.github.com>
2024-09-18 19:56:07 +0200

    more srtyles to completedPomodoros

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -49,7 +49,7 @@ const Header: React.FC<HeaderProps> = ({ pomodoroTime, setPomodoroTime, shortBre
             {/* Completed pomodoros */}
             <span className="flex items-center justify-center">
                 {/* TODO: Revisar els estils i si això realment ha de colocar-se aqui */}
-                <p className="text-xl font-medium mt-[6px] mr-4">You have completed <strong>{completedPomodoros}</strong> pomodoros today</p>
+                <p className="text-xl font-medium mt-1 mr-4">You have completed <strong className="text-2xl">{completedPomodoros}</strong> pomodoros today</p>
             </span>
 
             {/* Stats and Settings */}
